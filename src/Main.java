b2b2bc997c663cbbdec61e32e8ed9e1bf451c0d5
shuc0019 <shuc0019@stud.kea.dk>
2023-03-15 10:05:20 +0100

    sss

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.sql.SQLException;
 
 public class Main {
-
+//Hej
     public static void main(String[] args) throws SQLException {
 
        new Main().menu();
